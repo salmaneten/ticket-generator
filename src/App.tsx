b1281c7aa99a8272777d925376e464fb12d26389
Salmane <zerradisalmane2@gmail.com>
2025-04-23 19:45:18 +0100

    add z index for spacing at the bottom

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import BackgroundWrapper from "./components/BackgroundWrapper";
 function App() {
   return (
     <BackgroundWrapper>
-      <div className="max-w-4xl mx-auto pt-12 px-4 flex flex-col items-center justify-center">
+      <div className="max-w-4xl mx-auto pt-12 px-4 flex flex-col items-center justify-center z-20">
         <Title />
         <Form />
       </div>
